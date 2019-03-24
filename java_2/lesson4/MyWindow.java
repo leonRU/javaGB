82bd054c0362f340926b1e2ef68d91003bbf5ff6
leonRU <40966755+leonRU@users.noreply.github.com>
2019-03-25 01:26:27 +0300

    Up mywindow
    delete unused

--- java_2/lesson4/MyWindow.java
+++ java_2/lesson4/MyWindow.java
@@ -2,9 +2,6 @@ package lesson4;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 
 public class MyWindow extends JFrame {
 
