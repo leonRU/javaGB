7468dfffbbf5e6aa2b4810e5ba2a05bbd7a93d30
LeonRU <apa1993@mail.ru>
2019-03-25 01:20:56 +0300

    homework

---
+++ java_2/lesson4/MyWindow.java
@@ -0,0 +1,51 @@
+package lesson4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+public class MyWindow extends JFrame {
+
+    private JTextArea chatArea;
+    private JTextField messageArea;
+
+
+    public MyWindow(){
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(500, 100, 500, 500);
+
+        chatArea = new JTextArea();
+        chatArea.setEditable(false);
+        chatArea.setLineWrap(true);
+
+        JScrollPane scrollChatArea = new JScrollPane(chatArea);
+        add(scrollChatArea, BorderLayout.CENTER);
+
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        add(bottomPanel, BorderLayout.SOUTH);
+
+        messageArea = new JTextField();
+        bottomPanel.add(messageArea, BorderLayout.CENTER);
+
+        JButton sendMessage = new JButton("Send");
+        bottomPanel.add(sendMessage, BorderLayout.EAST);
+        sendMessage.addActionListener(e -> {
+            if(!messageArea.getText().isEmpty() && !messageArea.getText().startsWith(" ")) {
+                chatArea.append(messageArea.getText());
+                chatArea.append("\n");
+            }
+            messageArea.setText("");
+        });
+
+        bottomPanel.getRootPane().setDefaultButton(sendMessage);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new MyWindow();
+    }
+}
+
